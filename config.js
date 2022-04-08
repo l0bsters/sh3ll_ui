97881a8a31188e0d5fba2e2990942ba23236ae9a
Dane Fisher <hide4@comcast.net>
2022-04-08 10:55:26 -0400

    added config.js

---
+++ config.js
@@ -0,0 +1,102 @@
+<script>
+    var source = {
+        "name": "Brasil",
+            "children": [{
+            name: "Norte",
+            children: [{
+                name: "Acre",
+                    "size": 20
+            }, {
+                name: "Amapá",
+                    "size": 20
+            }, {
+                name: "Amazonas",
+                    "size": 251
+            }, {
+                name: "Pará",
+                    "size": 107
+            }, {
+                name: "Rondônia",
+                    "size": 33
+            }, {
+                name: "Roraima",
+                    "size": 7
+            }, {
+                name: "Tocantins",
+                    "size": 39
+            }]
+        }, {
+            name: "Nordeste",
+            children: [{
+                name: "Alagoas",
+                    "size": 60
+            }, {
+                name: "Bahia",
+                    "size": 512
+            }, {
+                name: "Ceará",
+                    "size": 553
+            }, {
+                name: "Maranhão",
+                    "size": 80
+            }, {
+                name: "Paraíba",
+                    "size": 89
+            }, {
+                name: "Pernambuco",
+                    "size": 671
+            }, {
+                name: "Piauí",
+                    "size": 36
+            }, {
+                name: "Rio Grande do Norte",
+                    "size": 263
+            }, {
+                name: "Sergipe",
+                    "size": 54
+            }]
+        }, {
+            name: "Centro Oeste",
+            children: [{
+                name: "Distrito Federal",
+                    "size": 484
+            }, {
+                name: "Goiás",
+                    "size": 332
+            }, {
+                name: "Mato Grosso",
+                    "size": 126
+            }, {
+                name: "Mato Grosso do Sul",
+                    "size": 91
+            }]
+        }, {
+            name: "Sudeste",
+            children: [{
+                name: "Espírito Santo",
+                    "size": 201
+            }, {
+                name: "Minas Gerais",
+                    "size": 935
+            }, {
+                name: "Rio de Janeiro",
+                    "size": 1059
+            }, {
+                name: "São Paulo",
+                    "size": 4447
+            }]
+        }, {
+            name: "Sul",
+            children: [{
+                name: "Paraná",
+                    "size": 704
+            }, {
+                name: "Rio Grande do Sul",
+                    "size": 742
+            }, {
+                name: "Santa Catarina",
+                    "size": 372
+            }]
+        }]
+    };
+</script>
